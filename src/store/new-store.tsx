d6d1ad666e5f8fd826bfc149772e595801461117
xuting12 <xuting12@58.com>
2020-12-21 17:23:59 +0800

    test

--- src/store/new-store.tsx
+++ src/store/new-store.tsx
@@ -3,6 +3,7 @@ import { useLocalStore } from "mobx-react-lite";
 const store: any = () => {
   const test_store = useLocalStore(() => ({
     num: 1,
+    test: "22",
   }));
   return test_store;
 };
